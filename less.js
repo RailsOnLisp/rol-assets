e1a0e19561b447ff173d8e0c9c541c56d9aaf7a3
Thomas de Grivel <thomas@lowh.net>
2015-06-23 00:49:23 +0200

    Lint JS.

--- less.js
+++ less.js
@@ -5,7 +5,7 @@ var less = require('less');
 var print_error = function (e) {
   less.writeError(e);
   process.exit(2);
-}
+};
 
 try {
   var opt = JSON.parse(rw.readFileSync('/dev/stdin', 'utf8'));
@@ -15,13 +15,13 @@ try {
       print_error(e);
     var css = tree.toCSS(opt.css);
     rw.writeFileSync('/dev/stdout', css, 'utf8');
-  }
+  };
 
   var parse_data = function (e, data) {
     if (e)
       print_error(e);
-    new(less.Parser)(opt.parser).parse(data, print_tree)
-  }
+    new(less.Parser)(opt.parser).parse(data, print_tree);
+  };
 
   rw.readFile(path.resolve(process.cwd(), opt.src), 'utf8', parse_data);
 } catch (e) {
